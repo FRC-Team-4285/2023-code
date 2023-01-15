bf6ad14c1f643d8efa40deb8caf36fc9404fcb11
Michael Wass <michael@mfwass.com>
2023-01-15 11:35:18 -0500

    general: add initial drive code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +18,58 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final class Swerve {
+
+    public static final SimpleMotorFeedforward driveFF = new SimpleMotorFeedforward(0.1, 0.15, 0.01);
+
+    /* Drivetrain Constants */
+    public static final double trackWidth = Units.inchesToMeters(19.5);
+    public static final double wheelBase = Units.inchesToMeters(16.5);
+
+    // nominal (real) divided by fudge factor
+    public static final double wheelDiameter = Units.inchesToMeters(4.0 / 1.04085);
+    public static final double wheelCircumference = wheelDiameter * Math.PI;
+
+    public static final double driveGearRatio = (6.75 / 1.0); // 6.75:1
+    public static final double angleGearRatio = ((150.0 / 7.0) / 1.0); // 150/7:1
+
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+                    new Translation2d(trackWidth / 2.0, wheelBase / 2.0), // front left
+                    new Translation2d(trackWidth / 2.0, -wheelBase / 2.0), // front right
+                    new Translation2d(-trackWidth / 2.0, wheelBase / 2.0), // rear left
+                    new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0) // rear right
+    );
+
+    /* Swerve Profiling Values */
+    public static final double maxSpeed = 4.5; // meters per second
+    public static final double maxAngularVelocity = 11.5;
+
+    public static final int frontLeftRotationMotorId = 8;
+    public static final int frontLeftDriveMotorId = 7;
+
+    public static final int frontRightRotationMotorId = 6;
+    public static final int frontRightDriveMotorId = 5;
+
+    public static final int rearLeftRotationMotorId = 2;
+    public static final int rearLeftDriveMotorId = 1;
+
+    public static final int rearRightRotationMotorId = 4;
+    public static final int rearRightDriveMotorId = 3;
+
+    public static final int frontLeftRotationEncoderId = 4;
+    public static final int frontRightRotationEncoderId = 1;
+    public static final int rearLeftRotationEncoderId = 3;
+    public static final int rearRightRotationEncoderId = 2;
+
+    public static final double kTeleDriveMaxSpeedMetersPerSecond = 7.5 / 4.0;
+    public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = 3.5;
+    public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 3;
+    public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 3;
+
+    public static final double cameraToFrontEdgeDistanceMeters = Units.inchesToMeters(7);
+
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,16 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.TeleopSwerve;
+import frc.robot.subsystems.SwerveBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+// import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,35 +25,49 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Joystick driver;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  /* Drive Controls */
+  private final int translationAxis = 1;
+  private final int strafeAxis = 0;
+  private final int rotationAxis = 4;
+
+  /* Drive Buttons */
+  private final JoystickButton zeroGyro;
+
+  /* Subsystems */
+  private final SwerveBase swerveBase;
+
+  public Joystick getDriver() {
+    return driver;
+  }
+
+  public SwerveBase getSwerveSubsytem() {
+    return swerveBase;
+  }
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    driver = new Joystick(0);
+    zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+    swerveBase = new SwerveBase();
+    swerveBase.setDefaultCommand(
+      new TeleopSwerve(
+        swerveBase,
+        () -> driver.getRawAxis(translationAxis),
+        () -> driver.getRawAxis(strafeAxis),
+        () -> driver.getRawAxis(rotationAxis),
+        () -> !driver.getRawButton(XboxController.Button.kLeftBumper.value)
+      )
+    );
+
+
     // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    zeroGyro.onTrue(new InstantCommand(() -> swerveBase.getNavX().reset()));
   }
 
   /**
@@ -56,8 +75,12 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  // public Command getAutonomousCommand() {
+  //   // An example command will be run in autonomous
+  //   return new SequentialCommandGroup(
+  //     new InstantCommand(() -> swerveBase.resetOdometry(0),
+  //     0
+  //     new InstantCommand(() -> swerveBase.stopModules()))
+  //   );
+  // }
 }
