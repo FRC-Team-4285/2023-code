8e6913dad473c9a41306061d8ed00106e4ef18b3
Michael Wass <michael@mfwass.com>
2023-01-28 10:14:11 -0500

    general: add initial autonomous code stub

---
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+// import java.io.IOException;
+
+public class AutonomousCommand extends CommandBase {
+
+    public AutonomousCommand() {
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() {
+        System.out.print("bla");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    public void end(boolean isInterrupted) {
+    }
+}
