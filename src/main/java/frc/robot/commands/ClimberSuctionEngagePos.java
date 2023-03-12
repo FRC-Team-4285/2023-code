45c83b212a2a3a96514f1d8c271342b0405c5a09
Michael Wass <michael@mfwass.com>
2023-03-12 16:35:51 -0400

    climber: maybe?

--- src/main/java/frc/robot/commands/ClimberSuctionEngagePos.java
+++ src/main/java/frc/robot/commands/ClimberSuctionEngagePos.java
@@ -24,7 +24,7 @@ public class ClimberSuctionEngagePos extends CommandBase {
   public ClimberSuctionEngagePos(ClimberArmBase climberSubsystem, SuctionCupBase suctionSubsystem) {
     m_climberSubsystem = climberSubsystem;
     m_suctionSubsystem = suctionSubsystem;
-    addRequirements(m_suctionSubsystem);
+    addRequirements(m_climberSubsystem);
   }
 
   private double getCurrentTime() {
